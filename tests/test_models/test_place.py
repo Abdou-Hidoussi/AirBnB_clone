a97e82b97e63d2576482d9015811df3c485b2c39
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-04 19:18:48 +0000

    more tests

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -5,29 +5,35 @@ import inspect
 import datetime
 import os
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
 import unittest
 
 
-class TestBaseModelMethods(unittest.TestCase):
+class TestPlaceMethods(unittest.TestCase):
 
     def test_id(self):
-        new = BaseModel()
+        new = Place()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        new = BaseModel()
+        new = Place()
         self.assertEqual(type(new.created_at),
                          datetime.datetime)
 
     def test_updated_at(self):
-        new = BaseModel()
+        new = Place()
         self.assertEqual(type(new.updated_at),
                          datetime.datetime)
 
     def test_str(self):
-        new = BaseModel()
+        new = Place()
         self.assertEqual(str(new), "[{:s}] ({:s}) {}".format(
             new.__class__.__name__, new.id, new.__dict__))
+
+    def test_name(self):
+        new = Place()
+        self.assertEqual(new.name, "")
 
 if __name__ == '__main__':
     unittest.main()
