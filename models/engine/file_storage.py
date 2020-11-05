db45117e1cc3b5db7065f231b7cfee6028c0edf7
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-05 17:55:20 +0000

    ???

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,23 +15,23 @@ from models.review import Review
 class FileStorage:
     """ storage Object
     """
-        __file_path = "file.json"
-        __objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         """ holder """
-        return self.__objects
+        return __objects
 
     def new(self, obj):
         """ holder """
-        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
+        __objects[obj.__class__.__name__ + "." + obj.id] = obj
 
     def save(self):
         """ holder """
         dic = {}
-        for key, value in self.__objects.items():
+        for key, value in __objects.items():
             dic[key] = value.to_dict()
-        with open(self.__file_path, 'w') as f:
+        with open(__file_path, 'w') as f:
             json.dump(dic, f)
         f.close()
 
@@ -43,9 +43,9 @@ class FileStorage:
             'Review': Review
             }
         data = {}
-        if(os.stat(self.__file_path).st_size is not 0):
-            with open(self.__file_path) as json_file:
+        if(os.stat(__file_path).st_size is not 0):
+            with open(__file_path) as json_file:
                 data = json.load(json_file)
                 for key, value in data.items():
-                    self.__objects[key] =\
+                    __objects[key] =\
                         idclasses[value['__class__']](**value)
