e63c576b804f6a6cf8e1b21545811eeaac02ad17
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-05 21:11:14 +0000

    ??

--- models/base_model.py
+++ models/base_model.py
@@ -14,8 +14,8 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """initializing an instance"""
         self.id = str(uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if (kwargs is not None):
             for key, value in kwargs.items():
                 if (key == "created_at" or key == "updated_at"):
@@ -36,7 +36,7 @@ class BaseModel:
         updates the public instance attribute
         updated_at with the current datetime
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         from models import storage
         storage.new(self)
         storage.save()
