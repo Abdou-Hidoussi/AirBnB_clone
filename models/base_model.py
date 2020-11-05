7c2ec041c6406e65ca8e5bdbeff491c7cef14462
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-05 19:12:34 +0000

    ???

--- models/base_model.py
+++ models/base_model.py
@@ -18,9 +18,8 @@ class BaseModel:
         self.updated_at = datetime.now()
         if (kwargs is not None):
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
+                if key is ("created_at" or "updated_at"):
                     Date_obj = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    self.__dict__[key] = Date_obj
                 else:
                     self.__dict__[key] = value
 
