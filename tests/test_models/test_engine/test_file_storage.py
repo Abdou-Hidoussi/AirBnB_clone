cdab40a52609479d5e4c9bd302109d37659519e4
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-05 21:27:45 +0000

    ????

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -11,6 +11,13 @@ import unittest
 
 class TestFileStorageMethods(unittest.TestCase):
 
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
     def test_base_model_save(self):
         new = BaseModel()
         new.save()
