46bf09580a3f7a26ac743f5d6f5239f6a9494d5d
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-05 19:29:40 +0000

    ???

--- models/base_model.py
+++ models/base_model.py
@@ -42,9 +42,8 @@ class BaseModel:
         """returns a dictionary containing"""
         new = self.__dict__.copy()
         new['__class__'] = self.__class__.__name__
-        if not(type(new['updated_at']) is str):
-            new['updated_at'] = self.updated_at.strftime(
-                "%Y-%m-%dT%H:%M:%S.%f")
-            new['created_at'] = self.created_at.strftime(
-                "%Y-%m-%dT%H:%M:%S.%f")
+        new['updated_at'] = self.updated_at.strftime(
+            "%Y-%m-%dT%H:%M:%S.%f")
+        new['created_at'] = self.created_at.strftime(
+            "%Y-%m-%dT%H:%M:%S.%f")
         return new
